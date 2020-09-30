7e898d0c21549cc464dea14ef659ee2fdb41d076
Asli Y <aslihancap@gmail.com>
2020-09-30 21:41:35 +0100

    changes

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,7 +2,10 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello");
+        System.out.println("Hello class EU3");
+
+        // more changes are coming
+        // i am ready to commit this
 
     }
 
